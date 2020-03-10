891b43e4280d2a03e14ba3cc41e9fceb49f38fb0
Kevin Garner <kevin.garner@ssigroup.com>
2020-03-10 15:41:05 -0500

    Fixed compilation error before moving on.

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sandbox',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SandboxComponent implements OnInit {
 
-  sandboxFormGroup: FormGroup
+  sandboxFormGroup: FormGroup;
 
   constructor() { }
 
