c924f6ee60b5ffc98b174c23c1459bfa84522bcf
Kevin Garner <kevin.garner@ssigroup.com>
2020-03-10 15:40:35 -0500

    Added sandbox.
    Added pretterrc file.

---
+++ src/app/sandbox/sandbox.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox',
+  templateUrl: './sandbox.component.html',
+  styleUrls: ['./sandbox.component.scss']
+})
+export class SandboxComponent implements OnInit {
+
+  sandboxFormGroup: FormGroup
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
