7c3019bf0e957d9bf907a9c269dab5400e8abe73
Kevin Garner <kevin.garner@ssigroup.com>
2020-03-10 17:04:23 -0500

    Started adding some reactive forms content to the sandbox.

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
-  styleUrls: ['./sandbox.component.scss']
+  styleUrls: ['./sandbox.component.scss'],
 })
 export class SandboxComponent implements OnInit {
-
   sandboxFormGroup: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const fb = new FormBuilder();
 
+    this.sandboxFormGroup = fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
 }
