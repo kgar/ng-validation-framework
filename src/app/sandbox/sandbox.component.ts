788c35ac8c2f47f014c11858dd506c90bc429320
Kevin Garner <kevin.garner@ssigroup.com>
2020-03-11 14:43:26 -0500

    Added some basic validation styles.
    Added validation decoration to the input.
    
    Added validation message provision and ordering.

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, MinLengthValidator } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-// TODO: To singleton service with multi->true, tokenized approach.
-const validationErrorMessageMap = new Map<string, (error) => string>();
-// TODO: Set up using tokenized approach
-validationErrorMessageMap.set('required', () => 'This field is required.');
-// TODO: Set up using tokenized approach
-validationErrorMessageMap.set(
-  'minlength',
-  error => `Minimum ${error.requiredLength} characters required.`,
-);
+interface ValidationMetadata {
+  errorMessage: ((error: any) => string) | string;
+  order: number;
+}
+
+/*
+  Interesting... Custom messages and order of error appearance are not a thing by default.
+  How do I make this a thing for my setup in a way that is extensible and easy to understand...?
+*/
+
+const validationMetadataMap = new Map<string, ValidationMetadata>();
+validationMetadataMap.set('required', { errorMessage: 'This field is required.', order: 1 });
+validationMetadataMap.set('minlength', {
+  errorMessage: error => `Minimum ${error.requiredLength} characters required.`,
+  order: 2,
+});
+
+interface KeyOrder {
+  key: string;
+  order: number;
+}
+
+function makeKeyOrder(key: string): KeyOrder {
+  return { key, order: validationMetadataMap.get(key).order };
+}
+
+function getHighestPriorityError(errors: object) {
+  if (!errors) {
+    return undefined;
+  }
+
+  const keys = Object.keys(errors);
+
+  let highestPriorityError: KeyOrder;
+  keys.forEach(key => {
+    const currentKeyOrder = makeKeyOrder(key);
+
+    if (highestPriorityError === undefined) {
+      highestPriorityError = currentKeyOrder;
+      return;
+    }
+
+    highestPriorityError = [currentKeyOrder, highestPriorityError].reduce(
+      entry => (entry.order < highestPriorityError.order ? entry : highestPriorityError),
+      highestPriorityError,
+    );
+  });
+
+  return highestPriorityError.key;
+}
 
 @Component({
   selector: 'app-sandbox',
@@ -18,6 +60,7 @@ validationErrorMessageMap.set(
 })
 export class SandboxComponent implements OnInit {
   sandboxFormGroup: FormGroup;
+  faInfoCircle = faInfoCircle;
   get nameControl() {
     return this.sandboxFormGroup.get('name');
   }
@@ -26,8 +69,13 @@ export class SandboxComponent implements OnInit {
     if (!errors) {
       return '';
     }
-    const topErrorType = Object.keys(errors)[0];
-    return validationErrorMessageMap.get(topErrorType)(this.nameControl.errors[topErrorType]);
+    const topErrorType = getHighestPriorityError(this.nameControl.errors);
+    const errorMessageProp = validationMetadataMap.get(topErrorType).errorMessage;
+    const errorMessage =
+      typeof errorMessageProp === 'function'
+        ? errorMessageProp(this.nameControl.errors[topErrorType])
+        : errorMessageProp;
+    return errorMessage;
   }
   get nameControlShowError() {
     return this.nameControl.invalid && this.nameControl.touched;
@@ -44,7 +92,6 @@ export class SandboxComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('submit attempted')
     this.sandboxFormGroup.markAllAsTouched();
 
     if (!this.sandboxFormGroup.valid) {
